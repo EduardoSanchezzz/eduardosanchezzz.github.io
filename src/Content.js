cdb843bb0dafed4b56427c03f53197473ab1ab3d
esanchezz74 <esanch747@gmail.com>
2022-12-21 14:25:45 -0500

    hero content

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import heroBackground from "./assets/images/Hero/heroBackground.svg";
+
+export const content = {
+    hero: {
+        title: "Web Developer",
+        firstName: "Sanchez",
+        LastName: "Eduardo",
+        btnText: "Contact Me",
+        image: heroBackground,
+        hero_content: [
+          {
+            text: "I design products that impress, improve, are stylish, are responsive",
+          },
+        ],
+      }
+
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,54 @@
+import { content } from "../Content";
 const Hero = () => {
-  return <div>Hero</div>;
+  const { hero } = content;
+
+  return (
+    <section id="home" className="overflow-hidden">
+      <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end items-center">
+        <div
+          data-aos="slide-left"
+          data-aos-delay="1200"
+          className="absolute h-full md:w-1/2 w-1/2 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
+        >
+        <div className="h-full">
+          <img
+            src={hero.image}
+            data-aos="slide-up"
+            alt="..."
+            className="h-full object-cover"
+          />
+        </div>
+          <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
+            {hero.firstName}{" "}
+            <span className="text-dark_primary">{hero.LastName}</span>
+          </h1>
+        </div>
+
+        {/* first col */}
+        <div className="pb-16 px-6 pt-5" data-aos="fade-down">
+          <h2>{hero.title}</h2>
+          <div className="flex flex-col gap-10 mt-10">
+            {hero.hero_content.map((content, i) => (
+              <div
+                key={i}
+                data-aos="fade-down"
+                data-aos-delay={i * 300}
+                className={`flex items-center w-80 gap-5
+            ${i === 1 && " flex-row-reverse text-right"}  `}
+              >
+                <p>{content.text}</p>
+              </div>
+            ))}
+          </div>
+          <br />
+          <div className="flex justify-end">
+            <button className="btn">{hero.btnText}</button>
+          </div>
+        </div>
+        
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
