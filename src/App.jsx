e24e17a850d3516abd1b58300484e9c60dca8f6e
esanchezz74 <esanch747@gmail.com>
2022-12-21 12:19:00 -0500

    Set up components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Service from "./components/Services";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <div className="">
+      <Hero />
+      <Skills />
+      <Service />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
